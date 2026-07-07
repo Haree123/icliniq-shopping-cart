2aa4c2478e92b9a42f78ddd72aa509324421253c
Haree <haree@addup.network>
2026-07-08 03:24:35 +0530

    chore(docker): add containerization, polishing and cleanups

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'server',
-
+  adapter: node({
+    mode: 'standalone',
+  }),
   security: {
     checkOrigin: false,
   },
-
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()],
   },
